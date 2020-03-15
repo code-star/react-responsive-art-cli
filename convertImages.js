7b07e730e1db9eff565c54018493deab4afcbe24
Mikel Arnaiz <mikel.arnaiz@philips.com>
2020-03-15 22:44:53 +0100

    Renames script name to convert-images

--- convertImages.js
+++ convertImages.js
@@ -1,14 +1,5 @@
 #!/usr/bin/env node
 
-function addNumber(a, b) {
-  return a + b;
-}
-
-console.log(process.argv);
-console.log(
-  "Sums: " + addNumber(Number(process.argv[3]), Number(process.argv[4]))
-);
-
 var formats = ["jpeg", "jp2", "webp"];
 var yargs = require("yargs");
 
