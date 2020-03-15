d516dae88ed20b487b31d0270e52db50c0015d5b
Mikel Arnaiz <mikel.arnaiz@philips.com>
2020-03-15 22:42:53 +0100

    Adds yargs

--- addNumber.js
+++ addNumber.js
@@ -8,3 +8,22 @@ console.log(process.argv);
 console.log(
   "Sums: " + addNumber(Number(process.argv[3]), Number(process.argv[4]))
 );
+
+var formats = ["jpeg", "jp2", "webp"];
+var yargs = require("yargs");
+
+var results = yargs
+  .option("size", {
+    alias: "s",
+    type: "array",
+    description: "Image size",
+    demandOption: true
+  })
+  .option("format", {
+    alias: "f",
+    type: "array",
+    choices: formats,
+    description: "Images format",
+    demandOption: true
+  }).argv;
+console.log(results);
